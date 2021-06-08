798df731e29b4d659081e54c810c4a5f5cda6226
Santiago Romero Peña <sromero6240@misena.edu.co>
2021-06-07 19:31:28 -0500

    join entre tabla cliente y usuario

--- Database.sql
+++ Database.sql
@@ -3,3 +3,8 @@ values ('5','Laurent', 'Rondon', 'CLL78 sur #23B23', '321355543', 'lvrondon8@mis
 
 insert into tienda.cliente (id, id_usuario, correo, num_documento, contraseña)
 values ('5', '5', 'lvrondon8@misena.edu.co', '10000764532', 'laurent123123');
+
+select *
+from tienda.cliente C
+join tienda.usuario U
+on U.id = C.id_usuario
