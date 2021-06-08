1c20c62a4a237c0e1c50dee92c622a91b6f0a5e2
Santiago Romero Peña <sromero6240@misena.edu.co>
2021-06-07 22:32:46 -0500

    tabla carrito

--- Database.sql
+++ Database.sql
@@ -57,5 +57,5 @@ on A.id = D.id
 comment 'visualiso los datos relacionados entre la tabla articulo e ingreso'
 
 insert into tienda.carrito (id, id_articulo, detalles, precios, articulo, imagenes, total)
-values ('4', '4', 'Los productos que has escogido para comprar y sus precios los podras vizualisar aca mismo',
-		'75.000', 'El Arte Contemporáneo en la Educación Artística', '', '75.000')
+values ('5', '5', 'Los productos que has escogido para comprar y sus precios los podras vizualisar aca mismo',
+		'32.000', 'LIBRO LA METAMORFOSIS BILINGUE', '', '32.000')
