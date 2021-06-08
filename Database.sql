63e93ab1f8ac7d4c1daf65a18f9f6227dbd6a095
Santiago Romero Peña <sromero6240@misena.edu.co>
2021-06-07 19:06:51 -0500

    correccion de ingreso de datos

--- Database.sql
+++ Database.sql
@@ -1,6 +1,3 @@
 insert into tienda.usuario (id, nombres, apellidos, direccion, numero_tel, correo, contraseña)
-values ('1','Santiago', 'Romero', 'CLL52 sur #93D98', '321388349', 'sromero@misena.edu.co', 'santiagoromero');
-values ('2','Andres', 'Rodriguez', 'CLL23 sur #94B34', '320355556', 'andresg@gmail.com', 'andresitoxd');
-values ('3','Helen', 'Chaparro', 'CLL12 sur #23A32', '321555566', 'hdchavarro9@misena.edu.co', 'helenchavaroo1233');
-values ('4','Luis', 'Peña', 'CLL52 sur #93C98', '31232543', 'luiseljose@gmail.com', 'luisromero23424');
-values ('5','Laurent', 'Rondon', 'CLL78 sur #23B23', '321355543', 'lvrondon8@misena.edu.co', 'laurent123123')
+values ('5','Laurent', 'Rondon', 'CLL78 sur #23B23', '321355543', 'lvrondon8@misena.edu.co', 'laurent123123');
+
