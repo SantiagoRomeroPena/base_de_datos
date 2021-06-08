fe60493afffd2ac3adf27fd48b65ad81d55c912b
Santiago Romero Peña <sromero6240@misena.edu.co>
2021-06-07 22:54:06 -0500

    tabla detalle_venta

--- Database.sql
+++ Database.sql
@@ -82,6 +82,6 @@ on C.id = V.id
 --'visualiza las uniones entres las tablas cliente y venta'
 
 insert into tienda.detalle_venta (id, id_venta, id_articulo, cantidad, precios, descuentos)
-values('3', '3', '3', '1', '75.000', '2.04')
+values('4', '4', '4', '1', '75.000', '1.04')
 
 
