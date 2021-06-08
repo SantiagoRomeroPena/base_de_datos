a76b4304da167583aa28bd9d164c5e343c7b56bd
Santiago Romero Peña <sromero6240@misena.edu.co>
2021-06-07 19:20:30 -0500

    datos de la tabla cliente

--- Database.sql
+++ Database.sql
@@ -1,3 +1,5 @@
 insert into tienda.usuario (id, nombres, apellidos, direccion, numero_tel, correo, contraseña)
 values ('5','Laurent', 'Rondon', 'CLL78 sur #23B23', '321355543', 'lvrondon8@misena.edu.co', 'laurent123123');
 
+insert into tienda.cliente (id, id_usuario, correo, num_documento, contraseña)
+values ('5', '5', 'lvrondon8@misena.edu.co', '10000764532', 'laurent123123');
