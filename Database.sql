c801d0070a0026d80bbea4cf7f81997ba1257ccb
Santiago Romero Peña <sromero6240@misena.edu.co>
2021-06-07 20:07:10 -0500

    datos de la tabla proveedor

--- Database.sql
+++ Database.sql
@@ -1,5 +1,5 @@
 insert into tienda.usuario (id, nombres, apellidos, direccion, numero_tel, correo, contraseña)
-values ('5','Laurent', 'Rondon', 'CLL78 sur #23B23', '321355543', 'lvrondon8@misena.edu.co', 'laurent123123');
+values ('6','sebastian', 'peralta', 'CLL78 sur #23B23', '321123456', 'sebas@gmail.com', 'elsebas');
 
 insert into tienda.cliente (id, id_usuario, correo, num_documento, contraseña)
 values ('5', '5', 'lvrondon8@misena.edu.co', '10000764532', 'laurent123123');
@@ -8,3 +8,6 @@ select *
 from tienda.cliente C
 join tienda.usuario U
 on U.id = C.id_usuario
+
+insert into tienda.proveedor (id, nombre, tipo_documento, numero_documento, direccion, telefono, email)
+values ('2', 'sebastian', 'C.C', '1000123321', 'CLL 23 sur #43D98', '321388324', 'sebasrodriguez@gmail.com')
